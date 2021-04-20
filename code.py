9c253da830038dd97f7601197ddd64556aed9ca4
asdasdads <teekan@protonmail.com>
2021-04-19 22:04:26 -0400

    asdasdadasda

--- code.py
+++ code.py
@@ -3,25 +3,52 @@ import time
 import os
 import subprocess
 from selenium.webdriver.firefox.options import Options
-opts = Options()
-opts.headless = True
+#opts = Options()
+#opts.headless = True
 path = os.getcwd()+"/geckodriver"
 def hit():
-	driver = webdriver.Firefox(options=opts,executable_path=path)
-	print(driver)
-	driver.get("http://www.slutbags.tk")
-	print(driver)
-	ads = driver.find_elements_by_tag_name("iframe")
-	print(ads)
-	ads[0].click()
+	try:
+		driver = webdriver.Firefox(executable_path=path)
+		driver.get("http://www.slutbags.tk")
+		ads = driver.find_elements_by_tag_name("iframe")
+	except Exception:
+		subprocess.run(["sudo","./kill-gecko"])
+
+	try:
+		ads[0].click()
+		time.sleep(3)
+		driver.switch_to.window(driver.window_handles[1])
+		driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+		time.sleep(1)
+		driver.execute_script("window.scrollTo(0,0)")
+		time.sleep(1)
+		driver.close()
+	except Exception:
+		subprocess.run(["sudo","./kill-firefox"])
+		subprocess.run(["sudo","./kill-gecko"])
+	try:
+		driver.switch_to.window(driver.window_handles[0])
+		time.sleep(1)
+		driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+		time.sleep(1)
+		driver.execute_script("window.scrollTo(0,0)")
+		time.sleep(1)
+		driver.close()
+		subprocess.run(["sudo","./delete-profiles"])
+	except Exception:
+		print("Didn't complete workflow")
+	"""
 	print("All tab after process")
 	print(driver.window_handles)
-	time.sleep(3)
-	for tab in driver.window_handles:
-		driver.switch_to.window(tab)
+	driver.switch_to.window(driver.window_handles[0])
+	tabs = driver.window_handles
+	rev_tabs = tabs[::-1]
+	for x in range(len(driver.window_handles)):
+		driver.switch_to.window(rev_tabs[x])
 		print("Closing " + driver.title)
 		driver.close()
-	subprocess.run(['sudo','./delete-profiles.sh'])
+	subprocess.run(['sudo','./delete-profiles'])
+	"""
 while True:
 	hit()
 
